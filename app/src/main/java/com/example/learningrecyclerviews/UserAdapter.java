ed49bb7ae18e60d8fd119c1d61f8d94974c81626
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-05-30 02:41:43 +0300

    Added functionality to onCreateViewHolder

--- app/src/main/java/com/example/learningrecyclerviews/UserAdapter.java
+++ app/src/main/java/com/example/learningrecyclerviews/UserAdapter.java
@@ -3,6 +3,7 @@ package com.example.learningrecyclerviews;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -27,7 +28,14 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserModelViewH
     @NonNull
     @Override
     public UserAdapter.UserModelViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        /*
+        This method constructs a RecyclerView.ViewHolder and sets the view it uses to display its contents.
+        The type of the ViewHolder must match the type declared in the Adapter class signature.
+         */
+//        Set the context
+        mContext = viewGroup.getContext();
+//        Return a ViewHolder which inflates a layout 'row_users'
+        return new UserModelViewHolder(LayoutInflater.from(mContext).inflate(R.layout.row_users, null));
     }
 
     @Override
