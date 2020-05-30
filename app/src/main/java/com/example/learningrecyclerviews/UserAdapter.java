4b9376e495f3dcf34ab2cc9c28d08bcf215e7036
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-05-30 15:09:06 +0300

    Implementing a Filter to the Adapter, overriding getFilter(), creating a list of Filtered results

--- app/src/main/java/com/example/learningrecyclerviews/UserAdapter.java
+++ app/src/main/java/com/example/learningrecyclerviews/UserAdapter.java
@@ -6,16 +6,21 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserModelViewHolder> {
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserModelViewHolder> implements Filterable {
     /*
     To feed all your data to the list that can be viewed, you must extend the RecyclerView.Adapter class.
     This object creates views for items, and replaces the content of some of the views with new data items when the original item is no longer visible.
      */
+//    Variable for the filter
+    private List<UserModel> mGetUserModelListFiltered;
 
 //    Variables for the constructor
     private List<UserModel> mUserModelList;
@@ -25,6 +30,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserModelViewH
     public UserAdapter(List<UserModel> userModelList, SelectedUser selectedUser) {
         this.mUserModelList = userModelList;
         this.mSelectedUser = selectedUser;
+        this.mGetUserModelListFiltered = userModelList;
     }
 
     @NonNull
@@ -63,6 +69,55 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserModelViewH
     public int getItemCount() {
 //        Find the size of the whole list - this size will be used to find the position of an individual data item that needs to be displayed
         return mUserModelList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        /*
+        A filterable class can have its data constrained by a filter.
+        A filter constrains data with a filtering pattern.
+        Filters are usually created by Filterable classes.
+        Filtering operations performed by calling filter(java.lang.CharSequence) or filter(java.lang.CharSequence, android.widget.Filter.FilterListener) are performed asynchronously.
+        When these methods are called, a filtering request is posted in a request queue and processed later.
+         */
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+//                Invoked in a worker thread to filter the data according to the constraint.
+                FilterResults filterResults = new FilterResults();
+
+                if (charSequence == null | charSequence.length() == 0){
+                    filterResults.count = mGetUserModelListFiltered.size();
+                    filterResults.values = mGetUserModelListFiltered;
+                } else {
+//                    Find the character being searched for
+                    String searchCharacter = charSequence.toString().toLowerCase();
+//                    Create a new Array List that will be based off the results of the search
+                    List<UserModel> resultData = new ArrayList<>();
+//                    Loop through the UserModel
+                    for (UserModel userModel: mGetUserModelListFiltered){
+//                        If a user has the same characters
+                        if(userModel.getUserName().toLowerCase().contains(searchCharacter)){
+//                            Add them to the result List
+                            resultData.add(userModel);
+                        }
+                    }
+//                    Change the filter results
+                    filterResults.count = resultData.size();
+                    filterResults.values = resultData;
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+//                Invoked in the UI thread to publish the filtering results in the user interface.
+                mUserModelList = (List<UserModel>) filterResults.values;
+//                Notify the Adapter that the data has changed
+                notifyDataSetChanged();
+            }
+        };
+        return filter;
     }
 
     public interface SelectedUser{
