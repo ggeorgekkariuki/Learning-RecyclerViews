41e2bbb719451e83880e765736516c6b010fae53
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-05-30 11:53:30 +0300

    Serialised the UserModel

--- app/src/main/java/com/example/learningrecyclerviews/UserModel.java
+++ app/src/main/java/com/example/learningrecyclerviews/UserModel.java
@@ -1,9 +1,12 @@
 package com.example.learningrecyclerviews;
 
-public class UserModel {
+import java.io.Serializable;
+
+public class UserModel implements Serializable {
     /*
-        This is a class that holds data objects and a constructor
-         */
+    This is a class that holds data objects and a constructor
+    To serialize an object means to convert its state to a byte stream so that the byte stream can be reverted back into a copy of the object.
+     */
 //    Set up constructor variables
     private String userName;
 
