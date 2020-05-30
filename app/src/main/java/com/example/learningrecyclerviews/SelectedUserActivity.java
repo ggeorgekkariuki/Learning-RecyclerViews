7e64ff8ad03daf63f0cb8d9ba53948809f469208
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-05-30 11:47:14 +0300

    Added a reference for the text view within the class

--- app/src/main/java/com/example/learningrecyclerviews/SelectedUserActivity.java
+++ app/src/main/java/com/example/learningrecyclerviews/SelectedUserActivity.java
@@ -2,12 +2,17 @@ package com.example.learningrecyclerviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SelectedUserActivity extends AppCompatActivity {
-
+//    Global variables
+    TextView tvSelectedUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_user);
+
+//        Find a reference to the TextView in the layout
+        tvSelectedUser = findViewById(R.id.tvSelectedUser);
     }
 }
