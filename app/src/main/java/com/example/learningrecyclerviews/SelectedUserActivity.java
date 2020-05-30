dc1a9845c46db1094dc0cf2b947731f7622edd01
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-05-30 11:29:57 +0300

    Resolved an Android Studio bug that caused java not to see a layout file by File>Invalidate and restart

---
+++ app/src/main/java/com/example/learningrecyclerviews/SelectedUserActivity.java
@@ -0,0 +1,13 @@
+package com.example.learningrecyclerviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectedUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_user);
+    }
+}
